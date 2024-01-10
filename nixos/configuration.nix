ef4e56e5086efcb31b7b9ad8763eae7fd7c88baf
Matthew Brunelle <ciferkey@gmail.com>
2024-01-10 16:29:11 -0500

    added steam

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -134,7 +134,8 @@
       firefox
       git
       kate
-      vim
+      neovim
+      xivlauncher
     ];
   };
 
@@ -151,4 +152,10 @@
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    gamescopeSession.enable = true;
+  };
 }
