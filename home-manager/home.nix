0b1dc76b978fce72884088464648233fe0234732
Matthew Brunelle <ciferkey@gmail.com>
2024-01-10 17:01:38 -0500

    updating to 23.11

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -49,13 +49,9 @@
 
   # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "ciferkey";
+    homeDirectory = "/home/cikerkey";
   };
-
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
@@ -65,5 +61,5 @@
   systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "23.05";
+  home.stateVersion = "23.05;
 }
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -151,7 +151,7 @@
   ];
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "23.11";
+  system.stateVersion = "23.05";
 
   programs.steam = {
     enable = true;
