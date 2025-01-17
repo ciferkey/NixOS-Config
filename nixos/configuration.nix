ca3f52ab3a0a573335d468ee9abe11dc3df8b909
Matthew Brunelle <ciferkey@gmail.com>
2025-01-17 12:40:15 -0500

    updated 2025/01-16

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -134,7 +134,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -224,7 +224,7 @@
   };
 
   # Fix EDID for monitor on AMD
-  hardware.display.edid.linuxhw."XL2420G_2014" = ["XL2420G" "2014"];
+  hardware.display.edid.linuxhw."XL2420G_2014" = ["BNQ7F39" "XL2420G" "2014" "2E7C0DC569A3"];
   hardware.display.outputs."DP-2".edid = "XL2420G_2014.bin";
   hardware.display.outputs."DP-2".mode = "e";
 
