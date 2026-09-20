42085f5c16ebd85a50936c864e10e3dd738e8994
Matthew Brunelle <ciferkey@gmail.com>
2026-09-20 10:00:55 -0400

    reenable claude todo tool

--- home-manager/terminal.nix
+++ home-manager/terminal.nix
@@ -44,6 +44,7 @@ in {
       env = {
         CLAUDE_CODE_AUTO_COMPACT_WINDOW = "1000000"; # https://github.com/anthropics/claude-code/issues/43989
         CLAUDE_CODE_ENABLE_AWAY_SUMMARY = "0";
+        CLAUDE_CODE_ENABLE_TODO_TOOLS="1";
         CLAUDE_CODE_DISABLE_EXPERIMENTAL_BETAS = "1";
         CLAUDE_CODE_NO_FLICKER = "1";
         DISABLE_INSTALLATION_CHECKS = "1"; # https://github.com/anthropics/claude-code/issues/17289
